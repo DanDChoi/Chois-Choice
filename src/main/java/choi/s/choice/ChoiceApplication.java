8b9dc2b00879193c0e0e0e019130f844f5038ef8
DanDChoi <showbegin622@gmail.com>
2022-10-29 15:48:38 +0900

    first commit

---
+++ src/main/java/choi/s/choice/ChoiceApplication.java
@@ -0,0 +1,13 @@
+package choi.s.choice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/choi/s/choice/ChoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package choi.s.choice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
