a0ffcfe8e0c6a65b4858088df1248dec4dfb7f23
DanDChoi <showbegin622@gmail.com>
2022-11-05 00:13:58 +0900

    ord entity

---
+++ src/main/java/choi/choice/domain/ord.java
@@ -0,0 +1,15 @@
+package choi.choice.domain;
+
+import lombok.Data;
+
+@Data
+public class ord {
+    private String ord_no;
+    private String ord_dt;
+    private String mbr_no;
+    private String cstm_nm;
+    private String cstm_email;
+    private String post_no;
+    private String base_addr;
+    private String detail_addr;
+}
