4ff267f79a2e078db9d4a980f1270514f1784fe2
DanDChoi <showbegin622@gmail.com>
2022-11-06 22:59:25 +0900

    good

---
+++ src/main/java/choi/choice/domain/good.java
@@ -0,0 +1,15 @@
+package choi.choice.domain;
+
+import lombok.Data;
+
+@Data
+public class good {
+
+    private String good_no;
+    private String good_nm;
+    private String sale_beg_date;
+    private String sale_end_date;
+    private String color_nm;
+    private String color_cd;
+
+}
