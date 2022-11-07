803b1c67aaa45937dc1cbfc78dfff29b1e7f7753
DanDChoi <showbegin622@gmail.com>
2022-11-07 22:21:48 +0900

    payEntity

---
+++ src/main/java/choi/choice/domain/pay.java
@@ -0,0 +1,13 @@
+package choi.choice.domain;
+
+import lombok.Data;
+
+@Data
+public class pay {
+
+    private String pay_no;
+    private String pay_mn_cd;
+    private String pay_dt;
+    private String ord_no;
+    private String deal_tp_cd;
+}
