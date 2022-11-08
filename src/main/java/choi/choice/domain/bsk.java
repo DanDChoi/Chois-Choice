7699757e225087552737e1b7334a3f32f021e558
DanDChoi <showbegin622@gmail.com>
2022-11-08 22:41:57 +0900

    bsk

---
+++ src/main/java/choi/choice/domain/bsk.java
@@ -0,0 +1,12 @@
+package choi.choice.domain;
+
+import lombok.Data;
+
+@Data
+public class bsk {
+
+    private String bsk_no;
+    private String mbr_no;
+    private String session_id;
+    private java.util.Date reg_dt;
+}
