e0d6ad023e2ce7551ad55fd084eedb7a295354be
DanDChoi <showbegin622@gmail.com>
2022-11-09 22:16:11 +0900

    evtEntity

---
+++ src/main/java/choi/choice/domain/evt.java
@@ -0,0 +1,13 @@
+package choi.choice.domain;
+
+import lombok.Data;
+
+@Data
+public class evt {
+
+    private String evt_no;
+    private String eve_nm;
+    private String evt_dscr;
+    private String evt_beg_dt;
+    private String evt_end_dt;
+}
