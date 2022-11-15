3c34cfcd99d53601356c88fa09b5be4fd8c24cb6
DanDChoi <showbegin622@gmail.com>
2022-11-15 22:26:30 +0900

    grd

---
+++ src/main/java/choi/choice/domain/mbr_grd.java
@@ -0,0 +1,12 @@
+package choi.choice.domain;
+
+import lombok.Data;
+
+@Data
+public class mbr_grd {
+
+    private String mbr_no; //회원번호
+    private String grd_beg_dt; //등급시작일
+    private String grd_end_dt; //등급종료일
+    private String reg_id; //등록자
+}
