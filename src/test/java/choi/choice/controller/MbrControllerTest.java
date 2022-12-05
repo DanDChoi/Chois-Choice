08d41c25d114286f895570b6a74e2f5c3b686222
DanDChoi <showbegin622@gmail.com>
2022-12-05 22:20:14 +0900

    mbvTest

---
+++ src/test/java/choi/choice/controller/MbrControllerTest.java
@@ -0,0 +1,23 @@
+package choi.choice.controller;
+
+import choi.choice.service.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(MbrController.class)
+public class MbrControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    private MemberService memberService;
+
+    @Test
+    public void join(){
+
+    }
+}
