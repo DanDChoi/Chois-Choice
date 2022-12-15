a94a2ca10da97a26b7557e7802a2256289e7025a
Dan Daehyun Choi <showbegin622@gmail.com>
2022-12-15 22:36:16 +0900

    mbrgrd

--- src/main/java/choi/choice/domain/mbr_grd.java
+++ src/main/java/choi/choice/domain/mbr_grd.java
@@ -1,15 +1,22 @@
 package choi.choice.domain;
 
-import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
-@Data
+@NoArgsConstructor
 @Entity
 public class mbr_grd {
 
-    private String mbr_no; //회원번호
-    private String grd_beg_dt; //등급시작일
-    private String grd_end_dt; //등급종료일
-    private String reg_id; //등록자
+    @Id
+    @Column(name = "mbr_no", unique = true)
+    private String mbrNo; //회원번호
+    @Column(name = "grd_beg_dt")
+    private String grdBegDt; //등급시작일
+    @Column(name = "grd_end_dt")
+    private String grdEndDt; //등급종료일
+    @Column(name = "reg_id")
+    private String regId; //등록자
 }
