51721e8606650582e6c0d2b2d045a391540bab5d
Dan Daehyun Choi <showbegin622@gmail.com>
2022-12-16 18:19:38 +0900

    “evt”

--- src/main/java/choi/choice/domain/evt.java
+++ src/main/java/choi/choice/domain/evt.java
@@ -1,11 +1,12 @@
 package choi.choice.domain;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Data
+@NoArgsConstructor
 @Entity
 public class evt {
 
