ae04433bdb685f999b694433684711c3ca913b18
dh.choi <dh.choi@isecommerce.co.kr>
2022-12-19 15:50:21 +0900

    editevt

--- src/main/java/choi/choice/domain/evt.java
+++ src/main/java/choi/choice/domain/evt.java
@@ -1,12 +1,13 @@
 package choi.choice.domain;
 
+import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class evt {
 
