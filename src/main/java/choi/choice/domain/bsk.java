0b6cfdb24706d6520035ddbd9b1415973d0b20c3
dh.choi <dh.choi@isecommerce.co.kr>
2022-12-19 15:48:14 +0900

    bskentity

--- src/main/java/choi/choice/domain/bsk.java
+++ src/main/java/choi/choice/domain/bsk.java
@@ -1,17 +1,27 @@
 package choi.choice.domain;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Data
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class bsk {
 
     @Id
-    private String bsk_no;
-    private String mbr_no;
-    private String session_id;
-    private java.util.Date reg_dt;
+    @Column(name = "bsk_no", unique = true)
+    private String bskNo;
+
+    @Column(name = "mbr_no")
+    private String mbrNo;
+
+    @Column(name = "session_id")
+    private String sessionId;
+
+    @Column(name = "reg_dt")
+    private java.util.Date regDt;
 }
