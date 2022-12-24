7db10906500cdc46052abdb5e371e66dbe38daf4
Dan <dan@Danui-MacBookAir.local>
2022-12-24 16:15:37 +0900

    mbr save

--- src/main/java/choi/choice/service/MemberServiceImpl.java
+++ src/main/java/choi/choice/service/MemberServiceImpl.java
@@ -1,14 +1,20 @@
 package choi.choice.service;
 
 import choi.choice.domain.mbr;
+import choi.choice.repository.MbrRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Service
 @AllArgsConstructor
 @Slf4j
 public class MemberServiceImpl implements MemberService{
+
+    @Autowired
+    private MbrRepository mbrRepository;
 
     @Override
     public mbr login(mbr mbr) {
@@ -16,8 +22,8 @@ public class MemberServiceImpl implements MemberService{
     }
 
     @Override
-    public mbr register(mbr mbr) {
-        return null;
+    public mbr register(@ModelAttribute mbr mbr) {
+        return mbrRepository.save(mbr);
     }
 }
 
