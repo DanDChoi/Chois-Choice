abc522f17d3ccd2d53865f38af45925c80e7f8f1
dh.choi <dh.choi@isecommerce.co.kr>
2023-01-10 17:53:32 +0900

    0110

--- src/main/java/choi/choice/service/MemberService.java
+++ src/main/java/choi/choice/service/MemberService.java
@@ -4,11 +4,12 @@ import choi.choice.domain.mbr;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 public interface MemberService {
 
-    mbr login(mbr mbr, HttpServletRequest request);
+    mbr login(mbr mbr, HttpServletRequest request, HttpSession session);
     mbr register(mbr mbr);
     Optional<mbr> findStringId(mbr mbr);
 }
