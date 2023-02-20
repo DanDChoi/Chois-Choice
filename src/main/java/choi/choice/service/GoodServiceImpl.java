6becd7aeb941a3c094ba95935ef8354a18c06db8
Dan <dan@Danui-MacBookAir.local>
2023-02-20 22:37:16 +0900

    0220

---
+++ src/main/java/choi/choice/service/GoodServiceImpl.java
@@ -0,0 +1,17 @@
+package choi.choice.service;
+
+import choi.choice.domain.good;
+
+import java.util.List;
+
+public class GoodServiceImpl implements GoodService{
+    @Override
+    public List<good> findAll() {
+        return null;
+    }
+
+    @Override
+    public good add(good good) {
+        return null;
+    }
+}
