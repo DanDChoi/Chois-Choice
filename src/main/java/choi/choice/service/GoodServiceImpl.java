9518dbc9c8b0a76b605348aa7017bc3b93d2654e
Dan <dan@Danui-noteubug.local>
2023-02-23 23:10:16 +0900

    0223

--- src/main/java/choi/choice/service/GoodServiceImpl.java
+++ src/main/java/choi/choice/service/GoodServiceImpl.java
@@ -1,18 +1,27 @@
 package choi.choice.service;
 
 import choi.choice.domain.good;
+import choi.choice.repository.GoodRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@AllArgsConstructor
 public class GoodServiceImpl implements GoodService{
+
+    private GoodRepository goodRepository;
     @Override
     public List<good> findAll() {
         return null;
     }
 
     @Override
-    public void add(good good)
-    {}
+    public void add(good good) {
+        goodRepository.save(good);
+    }
 
     @Override
     public good findByNo(String id) {
