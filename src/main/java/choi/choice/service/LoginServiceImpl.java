99363051f7e6aa990fcea1276f24913507914081
Dan <dan@Danui-noteubug.local>
2023-03-13 22:05:02 +0900

    0313

--- src/main/java/choi/choice/service/LoginServiceImpl.java
+++ src/main/java/choi/choice/service/LoginServiceImpl.java
@@ -4,7 +4,6 @@ import choi.choice.domain.Mbr;
 import choi.choice.repository.MbrRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
