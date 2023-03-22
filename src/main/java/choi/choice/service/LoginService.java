43f48646d92887fa548821d7b05bbd66d9e4d0e9
Dan <dan@Danui-noteubug.local>
2023-03-22 21:34:28 +0900

    0322

--- src/main/java/choi/choice/service/LoginService.java
+++ src/main/java/choi/choice/service/LoginService.java
@@ -2,9 +2,11 @@ package choi.choice.service;
 
 import choi.choice.domain.Mbr;
 
+import javax.servlet.http.HttpServletRequest;
 import java.security.NoSuchAlgorithmException;
 
 public interface LoginService {
 
     boolean login(Mbr mbr) throws NoSuchAlgorithmException;
+    void logout(HttpServletRequest request);
 }
