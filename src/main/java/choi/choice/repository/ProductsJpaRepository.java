2014fccf53ef8827f54331fead089ad3e5599365
Dan <danchoi.dev@gmail.com>
2023-05-20 17:19:16 +0900

    0520

--- src/main/java/choi/choice/repository/ProductsJpaRepository.java
+++ src/main/java/choi/choice/repository/ProductsJpaRepository.java
@@ -1,10 +1,19 @@
 package choi.choice.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 @Repository
 @Transactional
 public class ProductsJpaRepository implements ProductsRepository{
+
+    private final EntityManager em;
+
+    @Autowired
+    public ProductsJpaRepository(EntityManager em) {
+        this.em = em;
+    }
 }
