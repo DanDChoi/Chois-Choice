bfdc24850a35da3602b40ae8dca1365573b54bce
dh.choi <dh.choi@isecommerce.co.kr>
2023-05-23 12:34:46 +0900

    0523

--- src/main/java/choi/choice/service/OrderService.java
+++ src/main/java/choi/choice/service/OrderService.java
@@ -1,7 +1,15 @@
 package choi.choice.service;
 
 import choi.choice.domain.Ord;
+
+import java.util.List;
+
 public interface OrderService {
 
     void createOrd(Ord ord);
+
+    Ord findOne(String ordNo);
+    List<Ord> findAll();
+
+    void deleteOrd(Ord ord);
 }
