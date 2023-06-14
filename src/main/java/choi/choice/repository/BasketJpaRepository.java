26ae7c96e4936d3e28b140b6fca9c8991d2cd16d
Dan <danchoi.dev@gmail.com>
2023-06-14 22:02:26 +0900

    0614

--- src/main/java/choi/choice/repository/BasketJpaRepository.java
+++ src/main/java/choi/choice/repository/BasketJpaRepository.java
@@ -1,5 +1,6 @@
 package choi.choice.repository;
 
+import choi.choice.domain.Bsk;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -7,4 +8,8 @@ import javax.transaction.Transactional;
 @Repository
 @Transactional
 public class BasketJpaRepository implements BasketRepository{
+    @Override
+    public void add(Bsk bsk) {
+
+    }
 }
--- src/main/java/choi/choice/service/BasketServiceImpl.java
+++ src/main/java/choi/choice/service/BasketServiceImpl.java
@@ -1,9 +1,12 @@
 package choi.choice.service;
 
+import choi.choice.domain.Bsk;
 import choi.choice.repository.BasketRepository;
 import groovy.util.logging.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @Service
@@ -12,4 +15,9 @@ public class BasketServiceImpl implements BasketService{
 
     private final BasketRepository basketRepository;
     private final SessionManager sessionManager;
+
+    @Override
+    public void add(Bsk bsk, HttpServletRequest request) {
+        basketRepository.add(bsk);
+    }
 }
