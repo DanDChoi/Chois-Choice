1317879842863b8d3544eafb44c29d48b66c2f4e
Dan <danchoi.dev@gmail.com>
2023-07-17 21:39:28 +0900

    0717

--- src/main/java/choi/choice/repository/PayJpaRepository.java
+++ src/main/java/choi/choice/repository/PayJpaRepository.java
@@ -1,10 +1,29 @@
 package choi.choice.repository;
 
+import choi.choice.domain.Pay;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 @Repository
 @Transactional
-public class PayJpaRepository implements PayRepository{
+public class PayJpaRepository implements PayRepository {
+
+    private final EntityManager em;
+
+    public PayJpaRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public void create(Pay pay) {
+        em.persist(pay);
+    }
+
+    @Override
+    public Pay findByNo(String payNo) {
+        Pay pay = em.find(Pay.class, payNo);
+        return pay;
+    }
 }
