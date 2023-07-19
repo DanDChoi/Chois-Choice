b9fca9ef07a4cce79d84eae4def4462d97ba1d2d
dh.choi <dh.choi@isecommerce.co.kr>
2023-07-19 15:31:17 +0900

    0719

--- src/main/java/choi/choice/repository/PayJpaRepository.java
+++ src/main/java/choi/choice/repository/PayJpaRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
@@ -19,6 +20,16 @@ public class PayJpaRepository implements PayRepository {
     @Override
     public void create(Pay pay) {
         em.persist(pay);
+    }
+
+    @Override
+    public List<Pay> findPays(String ordNo) {
+        return null;
+    }
+
+    @Override
+    public void deletePay(String payNo) {
+
     }
 
     @Override
--- src/main/java/choi/choice/repository/PayRepository.java
+++ src/main/java/choi/choice/repository/PayRepository.java
@@ -2,10 +2,15 @@ package choi.choice.repository;
 
 import choi.choice.domain.Pay;
 
+import java.util.List;
+
 public interface PayRepository {
 
     void create(Pay pay);
 
     Pay findByNo(String payNo);
 
+    List<Pay> findPays(String ordNo);
+
+    void deletePay(String payNo);
 }
