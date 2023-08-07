ec2a859eb77e177d85564bb046eee7d124ba87e9
Dan <danchoi.dev@gmail.com>
2023-08-07 21:53:45 +0900

    0807

--- src/main/java/choi/choice/controller/OrderController.java
+++ src/main/java/choi/choice/controller/OrderController.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -29,8 +30,13 @@ public class OrderController {
         return "store";
     }
 
-    @GetMapping("/add")
+    @PostMapping("/add")
     public void createOrd(@ModelAttribute Ord ord, OrdGod ordGod, Good good, HttpServletRequest request){
         orderService.createOrd(ord, ordGod, good, request);
+    }
+
+    @GetMapping("/find")
+    public Ord findOrdByOrdNo (String ordNo){
+        return orderService.findOne(ordNo);
     }
 }
