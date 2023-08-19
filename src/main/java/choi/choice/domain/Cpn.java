042d8ec56eb6a8b86dfd9cc9305b27cc159ec13d
Dan <danchoi.dev@gmail.com>
2023-08-19 23:10:45 +0900

    0819

---
+++ src/main/java/choi/choice/domain/Cpn.java
@@ -0,0 +1,41 @@
+package choi.choice.domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cpn")
+@Getter
+@Setter
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Cpn {
+
+    @Id
+    @Column(name = "cpn_no", unique = true)
+    private String cpnNo;
+    @Column(name = "cpn_tp_cd")
+    private String cpnTpCd;
+    @Column(name = "cpn_dc_rate")
+    private int cpnDcRate;
+    @Column(name = "cpn_dc_amt")
+    private int cpnDcAmt;
+    @Column(name = "cpn_beg_dt")
+    private java.util.Date cpnBegDt;
+    @Column(name = "cpn_end_dt")
+    private java.util.Date cpnEndDt;
+    @Column(name = "regtr_id")
+    private String regtrId;
+    @Column(name = "reg_dt")
+    private java.util.Date regDt;
+    @Column(name = "udter_id")
+    private String udterId;
+    @Column(name = "udt_dt")
+    private java.util.Date udtDt;
+}
