0bd459a7e8c4184a653ff81d8d8ee0d2089f686d
Dan <danchoi.dev@gmail.com>
2023-11-05 21:01:54 +0900

    1105

--- src/main/java/choi/choice/controller/AdminController.java
+++ src/main/java/choi/choice/controller/AdminController.java
@@ -1,10 +1,13 @@
 package choi.choice.controller;
 
+import choi.choice.service.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @Controller
@@ -12,8 +15,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class AdminController {
 
+    SessionManager sessionManager;
+
     @GetMapping("/page")
-    public String adminPage(){
-        return "adminPage";
+    public String adminPage(HttpServletRequest request){
+        String mbrId = sessionManager.getSession(request).getMbrId();
+        if (mbrId.equals("admin")){
+            return "adminPage";
+        } else {
+            return "redirect:/";
+        }
     }
 }
