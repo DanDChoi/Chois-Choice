ad453e71240b8f297d02ea2851ebcf28edb96bdc
Dan <danchoi.dev@gmail.com>
2024-01-03 23:48:27 +0900

    0103

--- src/main/java/choi/choice/controller/CouponController.java
+++ src/main/java/choi/choice/controller/CouponController.java
@@ -1,12 +1,15 @@
 package choi.choice.controller;
 
 import choi.choice.domain.Cpn;
+import choi.choice.service.CouponService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -16,6 +19,8 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class CouponController {
 
+    @Autowired
+    private final CouponService couponService;
     @RequestMapping("/create")
     public String cpnCreatForm() {
         return "cpn/creat";
@@ -27,7 +32,8 @@ public class CouponController {
     }
 
     @RequestMapping(value = "/detail")
-    public Cpn cpnDetail(Cpn cpn, Model model, HttpServletRequest request) {
+    public Cpn cpnDetail(@RequestParam("cpnNo") String cpnNo,  Cpn cpn, Model model, HttpServletRequest request) {
+        Cpn cpn = couponService.couponDetail(cpnNo);
         return Cpn;
     }
 }
---
+++ src/main/java/choi/choice/service/CouponServiceImpl.java
@@ -0,0 +1,16 @@
+package choi.choice.service;
+
+import choi.choice.domain.Cpn;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CouponServiceImpl implements CouponService{
+
+    @Override
+    public Cpn couponDetail(String cpnNo) {
+        //TODO
+        return null;
+    }
+}
