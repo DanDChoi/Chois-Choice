10ecc6c514c81f8386cbed357f55d6d36251390b
dh.choi <dh.choi@isecommerce.co.kr>
2024-01-05 16:14:00 +0900

    0105

--- src/main/java/choi/choice/repository/CouponJpaRepository.java
+++ src/main/java/choi/choice/repository/CouponJpaRepository.java
@@ -2,10 +2,18 @@ package choi.choice.repository;
 
 import choi.choice.domain.Cpn;
 
+import javax.persistence.EntityManager;
+
 public class CouponJpaRepository implements CouponRepository{
+
+    private EntityManager em;
 
     @Override
     public Cpn cpnDetail(String cpnNo) {
-        return null;
+        String query = "select c from Cpn c where c.cpnNo = :cpnNo";
+        Cpn cpn = em.createQuery(query, Cpn.class)
+                .setParameter("cpnNo", cpnNo)
+                .getSingleResult();
+        return cpn;
     }
 }
