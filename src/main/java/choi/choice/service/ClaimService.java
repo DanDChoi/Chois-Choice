e519c4e5a70350e3d63097788f61644bc93a50ee
dh.choi <dh.choi@isecommerce.co.kr>
2024-02-16 17:59:41 +0900

    0216

--- src/main/java/choi/choice/service/ClaimService.java
+++ src/main/java/choi/choice/service/ClaimService.java
@@ -2,10 +2,14 @@ package choi.choice.service;
 
 import choi.choice.domain.*;
 
+import java.util.List;
+
 public interface ClaimService {
 
     void createClm(Ord ord, Clm clm, OrdGood ordGood);
     Clm findClm(String clmNo);
 
     void editClm(Clm clm, ClmGood clmGood);
+
+    List<Clm> clmList();
 }
