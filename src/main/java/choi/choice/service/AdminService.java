cf59b9b7e248ea9efd1a89203887c861f8fa17ca
Dan <danchoi.dev@gmail.com>
2024-03-15 23:01:58 +0900

    0315

--- src/main/java/choi/choice/service/AdminService.java
+++ src/main/java/choi/choice/service/AdminService.java
@@ -1,10 +1,19 @@
 package choi.choice.service;
 
+import choi.choice.domain.DspCtgry;
 import choi.choice.domain.StdCtgry;
 
 import java.util.List;
 
 public interface AdminService {
 
+    void addStdCtgry();
+
+    void addDspCtgry();
+
     List<StdCtgry> stdCtgryList();
+
+    List<DspCtgry> dspCtgryList();
+
+
 }
