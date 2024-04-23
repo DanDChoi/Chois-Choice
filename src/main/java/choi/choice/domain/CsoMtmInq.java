f79cab86a73a2386e720de73e18c6566928b9a84
dh.choi <dh.choi@isecommerce.co.kr>
2024-04-23 10:55:07 +0900

    0423

--- src/main/java/choi/choice/domain/CsoMtmInq.java
+++ src/main/java/choi/choice/domain/CsoMtmInq.java
@@ -8,34 +8,49 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "cso_mtm_inquiry")
+@Table(name = "cso_mtm_inq")
 @Getter
 @Setter
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class CsoMtmInquiry {
+public class CsoMtmInq {
 
     @Id
     @Column(name = "mtm_sn", unique = true)
     private String mtmSn;
+
+    @Column(name = "inq_tp_cd") //문의유형코드
+    private String inqTpCd;
+
     @Column(name = "good_no")
     private String good_no;
+
     @Column(name = "mbr_no")
     private Long mbrNo;
-    @Column(name = "inquiry_add_dt")
-    private java.util.Date inquiryAddDt;
-    @Column(name = "mtm_cont")
-    private String mtmCont;
-    @Column(name = "mtm_answer_yn")
-    private String mtmAnswerYn;
+
+    @Column(name = "mbr_nm")
+    private String mbrNm;
+
+    @Column(name = "inq_dt")
+    private java.util.Date inqDt;
+
+    @Column(name = "inq_cont")
+    private String inqCont;
+
+    @Column(name = "ans_stat_cd")
+    private String ansStatCd;
+
     @Column(name = "regtr_id")
     private String regtrId;
+
     @Column(name = "reg_dt")
     private java.util.Date regDt;
+
     @Column(name = "udter_id")
     private String udterId;
+
     @Column(name = "udt_dt")
     private java.util.Date udtDt;
 }
