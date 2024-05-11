9b6b1f1fefd61f1ec7b77883f373af73a837c0c4
Dan <danchoi.dev@gmail.com>
2024-05-11 19:48:36 +0900

    0511

--- src/main/java/choi/choice/domain/OrdGood.java
+++ src/main/java/choi/choice/domain/OrdGood.java
@@ -16,24 +16,31 @@ public class OrdGood {
 
     @EmbeddedId
     private OrdGodPK ordGodPK;
-    @Column(name = "ord_good_turn")
-    private int ordGoodTurn;
+
     @Column(name = "good_hist_no")
     private String goodHistNo;
+
     @Column(name = "good_nm")
     private String goodNm;
+
     @Column(name = "std_ctgry_no")
     private String stdCtgryNo;
+
     @Column(name = "sale_prc")
     private String salePrc;
+
     @Column(name = "dlv_plc_no")
     private Integer dlvPlcNo;
+
     @Column(name = "regtr_id")
     private String regtrId;
+
     @Column(name = "reg_dt")
     private java.util.Date regDt;
+
     @Column(name = "udter_id")
     private String udterId;
+
     @Column(name = "udt_dt")
     private java.util.Date udtDt;
 }
