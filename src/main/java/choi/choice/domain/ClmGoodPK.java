cd3b131bc8c7b3328777ca936a0f11496804d805
Dan <danchoi.dev@gmail.com>
2024-05-15 20:01:04 +0900

    0515

---
+++ src/main/java/choi/choice/domain/ClmGoodPK.java
@@ -0,0 +1,47 @@
+package choi.choice.domain;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class ClmGoodPK implements Serializable {
+
+    private final static long serialVersionUID = 1L;
+
+    private String clmNo;
+    private int clmGoodTurn;
+
+    public ClmGoodPK() {
+
+    }
+
+    public ClmGoodPK(String clmNo, int clmGoodTurn) {
+        this.clmNo = clmNo;
+        this.clmGoodTurn = clmGoodTurn;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+
+        ClmGoodPK clmGoodPK = (ClmGoodPK) obj;
+
+        if (this.clmNo.equals(clmGoodPK.clmNo) && this.clmGoodTurn == clmGoodPK.clmGoodTurn) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(clmNo, clmGoodTurn);
+    }
+}
