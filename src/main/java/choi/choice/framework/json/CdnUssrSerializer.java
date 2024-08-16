56e54207c5fc23cd34461cd4d2a696dd55672841
Dan <danchoi.dev@gmail.com>
2024-08-16 22:57:41 +0900

    0816

--- src/main/java/choi/choice/framework/json/CdnUssrSerializer.java
+++ src/main/java/choi/choice/framework/json/CdnUssrSerializer.java
@@ -1,5 +1,14 @@
 package choi.choice.framework.json;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class CdnUssrSerializer {
     private static final long serialVersionUID = -1L;
+    private ObjectMapper mapper;
+    private boolean useCdnUrl;
+
+    public CdnUssrSerializer(boolean useCdnUrl) {
+        this.useCdnUrl = useCdnUrl;
+        this.mapper = new ObjectMapper();
+    }
 }
